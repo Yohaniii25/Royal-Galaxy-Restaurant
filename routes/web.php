6167c7b6bae84a8fb26042b406048b529f9c0031
Yohaniii25 <yohanii725gmail.com>
2022-09-06 14:24:58 +0530

    added login, register and changed routes

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,14 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get("/home",[HomeController::class, "index"]);
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [HomeController::class, 'index']);
+
+Route::middleware(['auth:sanctum',
+    config('jetstream.auth_session'),'verified',])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
